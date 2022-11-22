10df81907425ed1f033cba9c15bdba7d2577cef6
Mateus Pier <codaverso@protonmail.com>
2022-11-22 12:48:02 -0300

    Versão Deploy

--- experiencein/experiencein/settings.py
+++ experiencein/experiencein/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')82*3da)=0(jjg$a-!yrwkmzt%lz88_8s6_e4_6ph5ldp^czs3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
